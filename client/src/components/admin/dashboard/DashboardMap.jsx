ba0d1be5cbb0ee2cf23ec2caa7272023cf4deb90
r2raw <martejrii.arturo.07172001@gmail.com>
2024-12-07 01:33:45 +0800

    env

--- client/src/components/admin/dashboard/DashboardMap.jsx
+++ client/src/components/admin/dashboard/DashboardMap.jsx
@@ -7,12 +7,16 @@ import {
 
 } from '@vis.gl/react-google-maps';
 
+
+
 const coords = { lat: 14.648282, lng: 121.049850 };
 function DashboardMap() {
 
+    console.log(import.meta.env.VITE_MAPS_API)
+
     const [open, setOpen]=useState(false);
     return (
-        <APIProvider apiKey='AIzaSyBnv-QuZZ2EuiQYp4kV7MQkz7Y2UZ2dF50'>
+        <APIProvider apiKey={import.meta.env.VITE_MAPS_API}>
             <div className='h-80 shadow-lg hover:shadow-none'>
                 <Map minZoom={10} maxZoom={0} center={coords} mapId={'94fc902b94e60099'}/>
                 <AdvancedMarker position={coords} onClick={()=> setOpen(true)}></AdvancedMarker>
